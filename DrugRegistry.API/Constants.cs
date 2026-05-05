2d0ea949146bf0f681dc70418150a0c77686068b
dimeskigj <dimeskigj@gmail.com>
2026-05-05 23:35:21 +0200

    chore: format Constants.cs

--- DrugRegistry.API/Constants.cs
+++ DrugRegistry.API/Constants.cs
@@ -3,9 +3,10 @@
 public static class Constants
 {
     public const string AppName = "DrugRegistry.API/0.1";
+
     public const string CsvUrl =
         "https://data.gov.mk/dataset/a930f47e-a059-4cbc-8c0b-83ca48fb234f/resource/ecff2aef-9c8e-4efd-a557-96df4fff9adb/download/lekovi.csv";
-    
+
     private const string LekoviWeb = "https://lekovi.zdravstvo.gov.mk";
     private const string NominatimGeocodingApi = "https://nominatim.openstreetmap.org";
     public static readonly Uri LekoviWebUrl = new(LekoviWeb);
